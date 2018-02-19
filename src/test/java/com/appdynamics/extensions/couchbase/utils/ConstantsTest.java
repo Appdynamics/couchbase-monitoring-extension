6b37c0ae37987780059e07dc55bea457790320ac
akshayAppd <akshay.srivastava@appdynamics.com>
2018-02-19 17:40:49 +0530

    v2.0.1 - Updated Licenses

--- src/test/java/com/appdynamics/extensions/couchbase/utils/ConstantsTest.java
+++ src/test/java/com/appdynamics/extensions/couchbase/utils/ConstantsTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2014. AppDynamics LLC and its affiliates.
+ *  * All Rights Reserved.
+ *  * This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ *  * The copyright notice above does not evidence any actual or intended publication of such source code.
+ */
+
 package com.appdynamics.extensions.couchbase.utils;
 
 import com.appdynamics.extensions.metrics.Metric;
